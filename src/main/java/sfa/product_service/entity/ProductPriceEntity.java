9c92862cda627e0f673f06592a91f96b59966552
Vivek Pandey <vivekpandey1464@gmail.com>
2024-09-10 00:28:21 +0530

    price data type change to flaot from double or int

--- src/main/java/sfa/product_service/entity/ProductPriceEntity.java
+++ src/main/java/sfa/product_service/entity/ProductPriceEntity.java
@@ -14,8 +14,8 @@ import lombok.experimental.FieldDefaults;
 @Table(name = "product_price_table")
 public class ProductPriceEntity extends BaseEntity{
     Long productId;
-    Double wareHousePrice;
-    Double stockListPrice;
-    Double retailerPrice;
-    Double gst;
+    float wareHousePrice;
+    float stockListPrice;
+    float retailerPrice;
+    float gst;
 }
