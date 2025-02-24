110c8739cc80c9748bb04ad12b108661169e07bb
Vivek Pandey <vivekpandey1464@gmail.com>
2025-02-24 19:11:05 +0530

    .

--- src/main/java/sfa/product_service/Configs/CorsConfig.java
+++ src/main/java/sfa/product_service/Configs/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*")
-                .allowedOrigins("https://www.prism-sfa-dev.net", "https://api.prism-sfa-dev.net", "http://localhost:3000")
+                .allowedOrigins("https://www.prism-sfa-dev.net", "https://api.prism-sfa-dev.net", "http://localhost:3000","https://dev.prism-sfa-dev.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*").allowCredentials(true)
                 .maxAge(3600);
