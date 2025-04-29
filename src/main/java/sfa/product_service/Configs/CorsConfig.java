4a66f410189251340857a7b569d77b5a3b18779e
Vivek Pandey <vivekpandey1464@gmail.com>
2025-04-29 15:58:53 +0530

    cors fxes

--- src/main/java/sfa/product_service/Configs/CorsConfig.java
+++ src/main/java/sfa/product_service/Configs/CorsConfig.java
@@ -13,7 +13,11 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://www.prism-sfa-dev.net", "https://staging.prism-sfa-dev.net/",
                         "http://staging.prism-sfa-dev.net/",
                         "http://88.222.215.37:9091",
-                        "http://88.222.215.37","https://api.prism-sfa-dev.net", "http://localhost:3000","https://dev.prism-sfa-dev.net","https://www.dev.prism-sfa-dev.net")
+                        "http://88.222.215.37","https://api.prism-sfa-dev.net", "http://localhost:3000","https://dev.prism-sfa-dev.net","https://www.dev.prism-sfa-dev.net",
+                        "https://test-pharma.prism-sfa-dev.net/",
+                        "https://www.test-pharma.prism-sfa-dev.net/",
+                        "https://test-fmcg.prism-sfa-dev.net/",
+                        "https://www.test-fmcg.prism-sfa-dev.net/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*").allowCredentials(true)
                 .maxAge(3600);
