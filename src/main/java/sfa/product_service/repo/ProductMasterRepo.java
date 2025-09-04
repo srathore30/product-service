7a86e9b102c81c665492ecc98b0d84d83104258e
kamini <patidarkamini05@gmail.com>
2025-09-04 12:32:34 +0530

    add query product repo

--- src/main/java/sfa/product_service/repo/ProductMasterRepo.java
+++ src/main/java/sfa/product_service/repo/ProductMasterRepo.java
@@ -3,6 +3,8 @@ package sfa.product_service.repo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sfa.product_service.entity.ProductMasterEntity;
 import sfa.product_service.entity.ProductPriceEntity;
@@ -12,6 +14,11 @@ import java.util.Optional;
 @Repository
 public interface ProductMasterRepo extends JpaRepository<ProductMasterEntity, Long> {
     Optional<ProductMasterEntity> findByProductCode(String productCode);
-    Page<ProductMasterEntity> findByName(String name, Pageable pageable);
-    Page<ProductMasterEntity> findBySku(String sku, Pageable pageable);
+
+    @Query("SELECT p FROM ProductMasterEntity p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    Page<ProductMasterEntity> findByName(@Param("name") String name, Pageable pageable);
+
+    @Query("SELECT p FROM ProductMasterEntity p WHERE LOWER(p.sku) LIKE LOWER(CONCAT('%', :sku, '%'))")
+    Page<ProductMasterEntity> findBySku(@Param("sku") String sku, Pageable pageable);
+
 }
